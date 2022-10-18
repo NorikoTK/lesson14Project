0a2e67e136f572f213e81e054e154f343ec042e4
st <baiv2004@gmail.com>
2022-10-18 21:36:36 +0300

    update

--- task04.py
+++ task04.py
@@ -2,6 +2,8 @@ BAD_MARK = "very bad"
 EXCELLENT_MARK = "Excllent"
 
 def get_feedback(mark):
+    if isinstance(mark, bool) or not isinstance(mark, int) or not (0 <= mark <= 10):
+        return -1
     result = EXCELLENT_MARK
     if mark <= 1:
         result = BAD_MARK
